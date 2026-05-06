557e8d211da4400cdf26f7feadc8796029cc7b07
evelezfajardo08 <evelezfajardo32@correo.unicordoba.edu.co>
2026-05-06 09:00:34 -0500

    add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BotsModule } from './bots/bots.module';
+import { UsersModule } from './users/users.module';
+import { ConversationsModule } from './conversations/conversations.module';
+import { ProgressModule } from './progress/progress.module';
+import { MessagesModule } from './messages/messages.module';
+import { ResultsModule } from './results/results.module';
+import { ModulesModule } from './modules/modules.module';
+import { ContentsModule } from './contents/contents.module';
+import { AssessmentsModule } from './assessments/assessments.module';
 
 @Module({
-  imports: [],
+  imports: [BotsModule, UsersModule, ConversationsModule, ProgressModule, MessagesModule, ResultsModule, ModulesModule, ContentsModule, AssessmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+ const config = new DocumentBuilder()
+    .setTitle('API de prueba')
+    .setDescription('Api de prueba')
+    .setVersion('1.0')
+    .addTag('prueba')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
